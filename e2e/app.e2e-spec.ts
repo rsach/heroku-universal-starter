147c208a9620dc6f62a1a3833b05565b746f92c7
rsach <rahul.sachdeva@live.com>
2017-07-06 23:18:28 +0530

    initial commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AbdPage } from './app.po';
+// import { AbdPage } from './app.po';
 
-describe('abd App', () => {
-  let page: AbdPage;
+// describe('abd App', () => {
+//   let page: AbdPage;
 
-  beforeEach(() => {
-    page = new AbdPage();
-  });
+//   beforeEach(() => {
+//     page = new AbdPage();
+//   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
-  });
-});
+//   it('should display welcome message', () => {
+//     page.navigateTo();
+//     expect(page.getParagraphText()).toEqual('Welcome to app!!');
+//   });
+// });
