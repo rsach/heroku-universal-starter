51f88daf356a585b8dcc5a702ab838420c48791e
rsach <rahul.sachdeva@live.com>
2017-07-06 23:36:49 +0530

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import * as express from 'express';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-const PORT = 80;
+const PORT = process.env.PORT || 8080;
 
 enableProdMode();
 
